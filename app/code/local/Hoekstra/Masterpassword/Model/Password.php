f1d2126c9ac7c45247ff44c827309629e5afc31f
Frans Hoekstra <frans@a1online.nl>
2014-02-04 16:36:42 +0100

    if the hash is the same, don't save

--- app/code/local/Hoekstra/Masterpassword/Model/Password.php
+++ app/code/local/Hoekstra/Masterpassword/Model/Password.php
@@ -5,10 +5,16 @@ class Hoekstra_Masterpassword_Model_Password extends Mage_Core_Model_Config_Data
     public function _beforeSave()
     {
         $value = $this->getValue();
+        $savedvalue = Mage::getStoreConfig('hoekstra/info/password');
         if($value) {
-            $hashed = Mage::getModel('customer/customer')->hashPassword($value);
+            if($value == $savedvalue) {
+                $hashed = $value;
+            } else {
+                $hashed = Mage::getModel('customer/customer')->hashPassword($value);
+            }
             $this->setValue($hashed);
         }
+
     }
 
 }
